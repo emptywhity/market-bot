fa029c13b5171c84bbd4305cc6e992168ed15351
emptywhity <99326402+emptywhity@users.noreply.github.com>
2025-06-06 22:47:58 +0200

    fix: add express server for Render

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function getCryptoNews() {
 client.once('ready', async () => {
   console.log('✅ Bot conectado a Discord');
 
-  cron.schedule('0 * * * *', async () => {
+  cron.schedule('* * * * *', async () => {
     const channel = await client.channels.fetch(channelId);
     const priceMsg = await getCryptoTrends();
     const newsMsg = await getCryptoNews();
